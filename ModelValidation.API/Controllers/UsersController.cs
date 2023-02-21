c22d357f2ace5d35e498c24a86eb4fd39a24fe62
Samuel-olurotimi <samuel.olurotimi@decagon.dev>
2023-02-21 09:31:47 +0100

    first commit

---
+++ ModelValidation.API/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidation.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ModelValidation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        //So we only use one end point becuase we are just get from the client only
+        // POST api/<UsersController>
+        [HttpPost]
+        public ActionResult<Users> Post([FromBody] Users user)
+        {
+            //Some Operation
+            return Ok(user);
+        }
+
+       
+    }
+}
---
+++ ModelValidation.API/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.API.Models
+{
+    public class Users
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression("^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$")] //Note remove the excape charaters afer copying from site e.g "\\"
+        public string Phone { get; set; }
+        [Required]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$")] 
+        public string Password { get; set; } // note because of the regex password must be a minimun of 8 character, one upper case, one lower case, 
+        // one number & one special character.
+    }
+}
+
+//so to have more control over model validation you can make use of regular expresion to get More regular expression you can go to   "iHateRegex"
